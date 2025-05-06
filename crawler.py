c5c18187a8dfd7c775fef4b0c48e2d5d080e27a6
donghunmm <106004105+donghunmm@users.noreply.github.com>
2025-05-06 23:08:13 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
@@ -38,7 +39,7 @@ def setup_driver():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    return webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 def crawl_saramin():
     driver = setup_driver()
